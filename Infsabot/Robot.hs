ae7a9232957501734d8c046b22a68c913fab69ea
Kavi Gupta <kavi@globalmbox.com>
2015-10-19 00:53:30 -0700

    Added documentation for Robot.hs

--- Infsabot/Robot.hs
+++ Infsabot/Robot.hs
@@ -1,5 +1,11 @@
 module Infsabot.Robot (
-        Robot(Robot), robotProgram,	robotTeam, robotAppearance, robotMaterial, robotBirthdate, robotMemory,
+        Robot(Robot),
+            robotProgram,
+            robotTeam,
+            robotAppearance,
+            robotMaterial,
+            robotBirthdate,
+            robotMemory,
         GameSpot(GameSpot), toSeenSpot,
         lineOfSight,
     ) where
@@ -11,18 +17,27 @@ import Infsabot.RobotAction
 -- This contains a Board Spot, which the Robot can always see, contains a robot's appearance iff there is a robot at that spot.
 data GameSpot = GameSpot BoardSpot (Maybe Robot)
 
+-- Converts a GameSpot to a seen spot
 toSeenSpot :: GameSpot -> SeenSpot
 toSeenSpot (GameSpot s Nothing) = SeenSpot s Nothing
 toSeenSpot (GameSpot s (Just rob)) = SeenSpot s $ Just $ robotAppearance rob
 
+-- Represents a game piece, called a Robot
 data Robot = Robot {
+        -- The program this Robot will use
 		robotProgram :: RobotProgram,
+        -- The team this robot belongs to
 		robotTeam :: Team,
+        -- How this robot appears to its surroundings
 		robotAppearance :: RobotAppearance,
+        -- The quantity of material this robot contains
 		robotMaterial :: Int,
+        -- The birthdate of this robot
         robotBirthdate :: Int,
+        -- The memory of this robot
 		robotMemory :: InternalState
 }
 
+-- The distance the given robot can see
 lineOfSight :: Robot -> Int
 lineOfSight _ = 1
