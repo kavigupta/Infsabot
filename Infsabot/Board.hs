89b5efa2781b9d493a01c941d6c5cac307c464da
Kavi Gupta <kavi@globalmbox.com>
2015-10-18 21:29:49 -0700

    Added state extractor

--- Infsabot/Board.hs
+++ Infsabot/Board.hs
@@ -3,6 +3,7 @@ module Infsabot.Board(BoardSpot, Board, startingBoard, renderBoard) where
 import Codec.Picture
 import Data.RandomAccessList
 import Infsabot.MathTools
+import Infsabot.Robot
 
 type RAL = RandomAccessList
 
@@ -10,19 +11,24 @@ data BoardSpot = SpotEmpty | SpotMaterial
 	deriving (Eq, Show)
 
 spotColor :: BoardSpot -> PixelRGB8
-spotColor SpotEmpty = PixelRGB8 255 255 255 
-spotColor SpotMaterial = PixelRGB8 128 128 128 
+spotColor SpotEmpty = PixelRGB8 255 255 255
+spotColor SpotMaterial = PixelRGB8 128 128 128
 
-data Board = Board { 
-	boardContents :: (RAL (RAL BoardSpot)),
-	boardRobots :: [Robot],
+data Board = Board {
+	boardContents :: (RAL (RAL (BoardSpot, Maybe Robot))),
+	boardRobots :: (Int, Int, Robot),
 	boardWidth :: Int,
-	boardHeight :: Int
+	boardHeight :: Int,
+	boardTime :: Int
 }
 
-(!!!) :: Board -> (Int, Int) -> BoardSpot
+(!!!) :: Board -> (Int, Int) -> (BoardSpot, Maybe Robot)
 b !!! (x, y) = boardContents b .!. x .!. y
 	where (.!.) = flip Data.RandomAccessList.lookup
+
+asSeenSpot :: (BoardSpot, Maybe Robot) -> SeenSpot
+asSeenSpot (boardSpot, Nothing) = SeenSpot {boardSpot = boardSpot, robotAt = Nothing}
+asSeenSpot (boardSpot, Just robot) = SeenSpot {boardSpot = boardSpot, robotAt = Just (robotAppearan	ce robot)}
 
 startingBoard :: Int -> Board
 startingBoard size = Board startingSpots size size
@@ -34,6 +40,19 @@ startingBoard size = Board startingSpots size size
 		initialColor :: Int -> Int -> BoardSpot
 		initialColor x y = if isPrime (x + y^2) then SpotMaterial else SpotEmpty
 
-renderBoard :: Board -> Image PixelRGB8
-renderBoard b = generateImage (\x y -> spotColor $ b !!! (x, y)) (boardWidth b) (boardHeight b)
+getKnownState :: Board -> (Int, Int, Robot) -> KnownState
+getKnownState b (x, y, rob) = KnownState {
+		peekAtSpot = peekFn,
+		material = robotMaterial rob,
+		stateLocation = (x,y),
+		stateAge = boardTime b - robotBirthdate rob,
+		stateMemory = robotMemory rob
+	}
+	where
+	peekFn :: (Offset, Offset) -> Maybe SeenSpot
+	peekFn (Offset x) (Offset y)
+		| x * x + y * y <= lineOfSight robot ^ 2	= Just $ asSeenSpot $ getKnownState b
+		| True										= Nothing
 
+renderBoard :: Board -> Image PixelRGB8
+renderBoard b = generateImage (\x y -> spotColor . fst $ b !!! (x, y)) (boardWidth b) (boardHeight b)
--- Infsabot/Robot.hs
+++ Infsabot/Robot.hs
@@ -1,17 +1,19 @@
-module Robot() where
+module Infsabot.Robot(Robot) where
 
-import Infsabot.Board
 import qualified Data.Map as M
 
--- Represents one of the 4 potential directions 
+-- Represents one of the 4 potential directions
 data Direction = N | E | W | S
 
 -- Represents a Team. Currently, there are only two teams.
 data Team = A | B
 
--- Represents a Spot on the Board as seen by a robot. 
+-- Represents a Spot on the Board as seen by a robot.
 -- This contains a Board Spot, which the Robot can always see, contains a robot's appearance iff there is a robot at that spot.
-data SeenSpot = SeenSpot {boardSpot :: BoardSpot, robotAt :: Maybe RobotAppearance}
+data SeenSpot = SeenSpot {
+    boardSpot :: BoardSpot,
+    robotAt :: Maybe RobotAppearance
+}
 
 -- Represents an offset from the original position.
 newtype Offset = Offset Int
@@ -24,31 +26,41 @@ data KnownState = KnownState {
 	-- Function the Robot can use to see around it. Returns nothing if the robot can't see that far.
 	peekAtSpot :: Offset -> Offset -> Maybe SeenSpot,
 	-- The material the robot is standing on
-	material :: Int, 
+	material :: Int,
 	-- The current location of the robot
 	stateLocation :: (Int, Int),
-	-- The current time, since robot's "birth"
-	stateTime :: Int,
+	-- The robot's age
+	stateAge :: Int,
 	-- The robot's memory
 	stateMemory :: InternalState
 }
 
 type RobotProgram = KnownState -> (RobotAction, InternalState)
 
-data RobotAction = MoveIn Direction | Dig | Spawn {
-	newProgram :: RobotProgram, 
-	newAppearance :: RobotAppearance,
-	newMaterial :: Int, 
-	newMemory :: InternalState
-}
+data RobotAction = MoveIn Direction
+                 | Dig
+                 | Spawn {
+                	newProgram :: RobotProgram,
+                	newAppearance :: RobotAppearance,
+                	newMaterial :: Int,
+                	newMemory :: InternalState
+                }
+                | Fire {
+                    fireDirection :: Direction,
+                    materialExpended :: Int
+                }
 
 data Robot = Robot {
 		robotProgram :: RobotProgram,
 		robotTeam :: Team,
 		robotAppearance :: RobotAppearance,
 		robotMaterial :: Int,
-		robotState :: InternalState
-	}
+        robotBirthdate :: Int,
+		robotMemory :: InternalState
+}
+
+lineOfSight :: Robot -> Int
+lineOfSight _ = 1
 
 data RobotAppearance = RobotAppearance {
 	robotColor :: (Int, Int, Int)
