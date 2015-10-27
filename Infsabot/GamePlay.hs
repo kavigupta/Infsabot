b2af6b6240e73032d8126c25fec658efd0566b19
Kavi Gupta <kavi@globalmbox.com>
2015-10-26 18:17:48 -0700

    Fixed logic error in removeCompetingMoves. Also some refactoring

--- Infsabot/GamePlay.hs
+++ Infsabot/GamePlay.hs
@@ -1,3 +1,5 @@
+module Infsabot.GamePlay(play) where
+
 import Infsabot.Board
 import Infsabot.Robot
 import Infsabot.RobotAction
@@ -12,7 +14,7 @@ type RobotAndAction = ((Int, Int, Robot), RobotAction)
 -- the main play function. This executes all robot actions and updates the board.
 play :: Parameters -> Board -> Board
 play p b =
-		-- apply move and spawn actions
+		-- apply actions
 		actionApplier .
 		-- apply all action costs
 		actionCostApplier .
@@ -22,42 +24,58 @@ play p b =
 		applyTimeTick $
 		b
 	where
-		-- all robots and robot program results
-		results = getRobotResults b
 		-- actions := results - state.
-		(hardDriveUpdater, actions) = updateAllHardDrives results
+		-- hardDriveUpdater updates the hard drive
+		(hardDriveUpdater, actions) = updateAllHardDrives $ getRobotResults b
+		-- resolves and sorts the actions
 		resolvedAndSortedActions
 			= sortBy (compare `on` (orderOfOperations . snd))
-				$ removeCompetingMoves actions
-		actionCostApplier = applyActionCosts p actions
-		actionApplier = foldl (.) id $ map (applyDNSF p) resolvedAndSortedActions
+				$ removeCompetingMoves
+				$ map (possibleAction p) actions
+		-- gets the function which applies the costs of the actions
+		actionCostApplier = applyActionCosts p resolvedAndSortedActions
+		-- gets the function which applies all actions
+		actionApplier = foldl (.) id $ map (getAction p) resolvedAndSortedActions
 
-applyDNSF :: Parameters -> RobotAndAction -> Board -> Board
-applyDNSF _ ((x,y,_),Die) b 	= deleteRobot (x, y) b
-applyDNSF _ (_, Noop) b		 	= b
-applyDNSF _ (xyrob, send@(SendMessage _ _)) b
-								= mutateRobot xyrob sendAction (sendDirection send) b
+-- gets the action associated with the given robotandaction in the form
+-- of a function that mutates a board
+getAction :: Parameters -> RobotAndAction -> Board -> Board
+getAction _ ((x,y,_),Die) b 		= deleteRobot (x, y) b
+getAction _ (_, Noop) b		 	= b
+getAction _ ((x, y, rob), send@(SendMessage _ _)) b
+									= mutateRobot
+										(x, y)
+										(sendDirection send)
+										(lineOfMessageSending rob)
+										sendAction
+										b
 	where
-	sendAction toReceive 		= Just $ toReceive { robotMessages = newMessage: robotMessages toReceive }
+	sendAction toReceive
+			= Just $ toReceive { robotMessages = newMessage: robotMessages toReceive }
 		where
 		newMessage :: (String, Direction)
 		newMessage = (messageToSend send, oppositeDirection $ sendDirection send)
-applyDNSF _ (xyrob, fire@(Fire _ _)) b
-								= mutateRobot xyrob fireAction (fireDirection fire) b
+getAction _ ((x, y, rob), fire@(Fire _ _)) b
+									= mutateRobot
+										(x, y)
+										(fireDirection fire)
+										(lineOfFire rob)
+										fireAction
+										b
 	where
 	fireAction toReceive
-			| newHP > 0 		= Just $ toReceive { robotHitpoints = newHP }
-			| otherwise			= Nothing
+			| newHP > 0 	= Just $ toReceive { robotHitpoints = newHP }
+			| otherwise		= Nothing
 		where newHP = robotHitpoints toReceive - hitpointsRemoved (materialExpended fire)
-applyDNSF _ ((x, y, _), Dig) b
-		| mat == SpotMaterial	= updateSpot (x,y) SpotEmpty b
-		| otherwise				= b
+getAction _ ((x, y, _), Dig) b
+		| mat == SpotMaterial		= updateSpot (x,y) SpotEmpty b
+		| otherwise					= b
 		where GameSpot mat _ = b !!! (x, y)
-applyDNSF _ ((x, y, rob), MoveIn dir) b
-								= deleteRobot (x, y) $ setRobot (newx, newy, rob) b
+getAction _ ((x, y, rob), MoveIn dir) b
+									= deleteRobot (x, y) $ setRobot (newx, newy, rob) b
 	where (newx, newy) = applyOffset (getOffset dir) (x, y)
-applyDNSF params ((x, y, rob), spawn@(Spawn _ _ _ _ _)) b
-								= setRobot (newx, newy, newRobot) b
+getAction params ((x, y, rob), spawn@(Spawn _ _ _ _ _)) b
+									= setRobot (newx, newy, newRobot) b
 	where
 	newRobot = Robot {
 		robotProgram = newProgram spawn,
@@ -71,55 +89,66 @@ applyDNSF params ((x, y, rob), spawn@(Spawn _ _ _ _ _)) b
 	}
 	(newx, newy) = applyOffset (getOffset $ newDirection spawn) (x, y)
 
-mutateRobot :: (Int, Int, Robot) -> (Robot -> Maybe Robot) -> Direction -> Board -> Board
-mutateRobot (x, y, rob) mutator direction = individualAction
+-- Gets a function that mutates a robot along a path, given
+	-- an original position
+	-- a direction to move in
+	-- a maximum number of paces
+	-- a function that takes a robot and returns
+		-- Just (the updated robot)
+		-- Nothing (the robot will be deleted)
+mutateRobot :: (Int, Int) -> Direction -> Int -> (Robot -> Maybe Robot) -> Board -> Board
+mutateRobot (x, y) direction distance mutator b
+		= case maybeRobot of
+			Just (_, _, toReceive) 	->
+				case (mutator toReceive) of
+					Just newRobot 		-> setRobot (x,y,newRobot) b
+					Nothing				-> deleteRobot (x, y) b
+			Nothing					-> b
 	where
-	individualAction b
-			= case maybeRobot of
-				Just (_, _, toReceive) 	->
-					case (mutator toReceive) of
-						Just newRobot 		-> setRobot (x,y,newRobot) b
-						Nothing				-> deleteRobot (x, y) b
-				Nothing					-> b
-		where
-		maybeRobot = robotAlongPath b (x, y) direction (lineOfMessageSending rob)
+	maybeRobot = robotAlongPath b (x, y) direction distance
 
--- Given a robot and action, gets a list containing ((oldx, oldy), (newx, newy))
-finalLocations :: RobotAndAction -> [(Int, Int)]
-finalLocations ((x,y,_), act) = locs act
-	where
-	locs (MoveIn dir) = [applyOffset (getOffset dir) (x,y)]
-	locs spawn@(Spawn _ _ _ _ _)
-		= [applyOffset (getOffset $ newDirection spawn) (x,y)]
-	locs Die = []
-	locs _ = [(x,y)]
-
+--	Removes any moves that would result in two robots being in the same spot.
+--	Whichever move is a movement will be removed. If both are movements, both
+ 	-- are removed
 removeCompetingMoves :: [RobotAndAction] -> [RobotAndAction]
 removeCompetingMoves [] = []
-removeCompetingMoves (x:xs)
-		| works			= x : pureRest
+removeCompetingMoves (move:remainder)
+		| works			= move : pureRest
 		| otherwise 	= pureRest
 	where
-	(works, rest) = competingMove x xs
+	(works, rest) = removeCompetitionTo move remainder
 	pureRest = removeCompetingMoves rest
-
--- Removes all moves that compete with the given move.
--- Outputs False if the currently processed move should be removed, True otherwise
-competingMove :: RobotAndAction -> [RobotAndAction] -> (Bool, [RobotAndAction])
-competingMove current other
-	| any canConflict robAndLocOther			= (True, other)
-	| otherwise									= (False, map fst $ nonConflicting)
+	-- Removes all moves that compete with the given move.
+	-- Outputs False if the currently processed move should be removed, True otherwise
+	removeCompetitionTo :: RobotAndAction -> [RobotAndAction] -> (Bool, [RobotAndAction])
+	removeCompetitionTo current@((xcur,ycur,_), _) other
+			| any (fst . fault) robAndLocOther			= (True, other)
+			| otherwise									= (False, map fst $ nonConflicting)
 		where
 		nonConflicting :: [(RobotAndAction, (Int, Int))]
-		nonConflicting = filter (not . canConflict) robAndLocOther
+		nonConflicting = filter (not . snd . fault) robAndLocOther
 		locThis :: [(Int, Int)]
 		locThis = finalLocations current
 		robAndLocOther :: [(RobotAndAction, (Int, Int))]
 		robAndLocOther = concat $ map (\(raa, xys) -> map (raa,) xys) robAsscLocs
 			where
 			robAsscLocs = zip other $ map finalLocations other
-		canConflict :: (RobotAndAction, (Int, Int)) -> Bool
-		canConflict (_, xy) = any (== xy) locThis
+		fault :: (RobotAndAction, (Int, Int)) -> Conflict
+		fault (_, xy)
+			| xy == (xcur, ycur) 			= (False, True)
+			| otherwise						= (any (==xy) locThis, False)
+		finalLocations :: RobotAndAction -> [(Int, Int)]
+		finalLocations ((x,y,_), act) = locs act
+			where
+			locs (MoveIn dir) = [applyOffset (getOffset dir) (x,y)]
+			locs spawn@(Spawn _ _ _ _ _)
+				= [applyOffset (getOffset $ newDirection spawn) (x,y)]
+			locs Die = []
+			locs _ = [(x,y)]
+
+-- first one is if my fault, second is if other fault
+type Conflict = (Bool, Bool)
+
 -- Takes a list of robots and results and outputs
 	-- (a function that updates a board to one with hard drives updated,
 	-- 	a list of robots and their actions)
@@ -129,7 +158,7 @@ updateAllHardDrives rars = (
 		foldr (.) id $ map updateHardDrive rars,
 		map removeState rars)
 	where
-	removeState ((x,y,rob), (act, _)) = ((x,y,rob), act)
+	removeState (xyrob, (act, _)) = (xyrob, act)
 	updateHardDrive ((x,y,rob), (_, state)) = setRobot (x,y,rob {robotMemory = state})
 
 -- Takes a parameter list and list of actions, and applies all their costs to
@@ -170,22 +199,22 @@ getKnownState b (x, y, rob) = KnownState {
 -- Returns the closest approximation to the requested action that is possible
     -- given the robot's level of material
 -- This may be another type of action.
-possibleAction :: Parameters -> Robot -> RobotAction -> RobotAction
-possibleAction p rob action
-    | actionCost p action <= robotMaterial rob  = action
+possibleAction :: Parameters -> RobotAndAction -> RobotAndAction
+possibleAction p (xyrob@(_, _, rob), action)
+    | actionCost p action <= robotMaterial rob  = (xyrob, action)
     | otherwise 								= downgrade action
 	where
-	downgrade :: RobotAction -> RobotAction
-	downgrade Die = Die
-	downgrade Noop = Die -- no alternative
+	downgrade :: RobotAction -> RobotAndAction
+	downgrade Die = (xyrob, Die)
+	downgrade Noop = (xyrob, Die) -- no alternative
 	downgrade (MoveIn _) = tryNoop
 	downgrade Dig = tryNoop
 	downgrade s@(Spawn _ _ _ _ _) -- TODO Potential massive inefficiency here!
-		= possibleAction p rob $ s {newMaterial = newMaterial s - 1}
+		= possibleAction p (xyrob, s {newMaterial = newMaterial s - 1})
 	downgrade f@(Fire _ _)
-		= possibleAction p rob $ f {materialExpended = materialExpended f - 1}
+		= possibleAction p (xyrob, f {materialExpended = materialExpended f - 1})
 	downgrade (SendMessage _ _) = tryNoop
-	tryNoop = possibleAction p rob Noop
+	tryNoop = possibleAction p (xyrob, Noop)
 
 hitpointsRemoved :: Int -> Int
 hitpointsRemoved matExpend = 2 + matExpend
